030696b06ab021a733d3ed7c5f7c456ea530a4e0
mbron770 <mbronfin.tsd@gmail.com>
2023-06-15 22:07:39 -0400

    complete flow

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+
+function Child({ onChangeColor, color }) {
+  console.log(onChangeColor)
+  function handleClick(){
+    const newColor = getRandomColor();
+    onChangeColor(newColor)
+
+    
+
+  
+  }
+
+
+
+  
+
+
+  return <div onClick = 
+  {handleClick} style ={{backgroundColor: color}} 
+  className="child" />;
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,30 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState('#FFF')
+  
+  
+  
+  
+  function handleChangeColor(newChildColor){
+    //const newRandomColor = getRandomColor();
+    setColor(getRandomColor())
+    setChildrenColor(newChildColor);
+  }
 
+  
+
+  
+
+
+  
+  
+  
+  
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color = {childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color = {childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
